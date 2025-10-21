1ab57adb99679dacdbb1d059b738cef94c8c9d3d
TechMastergaurav <mtechgaurav03@gmail.com>
2025-10-21 06:23:51 +0530

    real time chats implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,12 @@ import messageRoutes from "./routes/message.route.js"
 import cookieParser from "cookie-parser"
 import path from "path"
 import { connectDb } from "./lib/db.js"
+import cors from "cors"
+import { app, server } from "./lib/socket.js"
 dotenv.config();
 
-const app = express();
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(cookieParser());
 const __dirname = path.resolve();
 const PORT = process.env.PORT||5000;
@@ -20,7 +22,7 @@ if(process.env.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
     })
 }
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectDb();
   console.log(`Server running on port ${PORT}`);
   
